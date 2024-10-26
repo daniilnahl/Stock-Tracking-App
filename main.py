51c8ff4779c4ce643af64892d7d7f38bd2a8829e
daniilnahl <daniil.nahl@protonmail.com>
2024-10-26 12:49:05 -0700

    v0.00.0.2
    API key import from .env works.
    Added a function to stock class to get basic info on the stock.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv #needed to change python interperter to the virtual one
-import stock
+from stock import Stock
 
 load_dotenv()
 API_KEY = os.getenv("MY_API_KEY")
@@ -10,10 +10,12 @@ API_KEY = os.getenv("MY_API_KEY")
 def main():
     print("Loading environment variables...")
     print("Environment variables loaded.")
-    
-    
-    
     print(API_KEY)
+    #WORKS NOW
+    
+    stock = Stock('AMZN', API_KEY)
+    stock.get_company_overview()
+    print(stock)
     
     
 if __name__=="__main__":
--- stock.py
+++ stock.py
@@ -1,12 +1,36 @@
 from dataclasses import dataclass
+import requests 
 
 @dataclass
 class Stock:
+    #needed
     ticker_symbol: str
-    industry: str = None
-    price: float = None
+    API_KEY: str
+    #stock price
+    open_price: float = None
+    close_price: float = None
+    low_price: float = None
+    high_price: float = None
+    #other info
+    sector: str = None
     market_cap: float = None
     eps: float = None #earning per share
     pe: float = None #price to earning
     
+    #gets basic stock info
+    def get_company_overview(self):
+        url = f"https://www.alphavantage.co/query?function=OVERVIEW&symbol={self.ticker_symbol}&apikey={self.API_KEY}"
+        r = requests.get(url)
+        data = r.json() #converts json into dictionary
+        type(data)#chekcs for type 
+        
+        #sets all of the values for the stock
+        self.sector = data['Sector']
+        self.market_cap = data['MarketCapitalization']
+        self.eps = data['EPS']
+        self.pe = data['PERatio']
+        
+    #gets daily price info
+    
+        
     
