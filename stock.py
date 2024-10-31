87021d34fd34f68179d265f4c7beed42d8acb7c6
daniilnahl <daniil.nahl@protonmail.com>
2024-10-31 12:40:04 -0700

    v.0.00.0.8
    Resolved file csv reading/writing bug where it would break up a ticker by commas.
    Created a utils package and moved ticker file module there.
    Created a loop to search if a ticker is valid.

--- stock.py
+++ stock.py
@@ -1,8 +1,9 @@
+#CLASS FOR A STOCK 
 from dataclasses import dataclass
 from urllib.request import urlopen
 import json
 import certifi
-import tickers_file_module as tfm
+import utils.tickers_file_module as tfm
 
 @dataclass
 class Stock:
@@ -50,25 +51,24 @@ class Stock:
     def check_ticker(self):
         valid_tickers_list = []
         tfm.read_file(valid_tickers_list)#store valid tickers into a list variable
-        print(valid_tickers_list)
         
-        if self.ticker_symbol not in valid_tickers_list:
-            print('ticker not on file')
-            
-            #uses api search to see if the ticker exists 
-            url = (f"https://financialmodelingprep.com/api/v3/search-ticker?query={self.ticker_symbol}&limit=10&exchange=NASDAQ&apikey={self.API_KEY}")
-            data = Stock.get_jsonparsed_data(url)
-            
-            print(data)
-            
-            if data == [ ]:#returns [ ] list if cant find a ticker
-                print('data is empty')
-                return False
-            else:#ONLY RECORDS THE LAST TICKER THAT WAS CALLED FIx THIS
-                tfm.write_file(self.ticker_symbol)
-                return True
-        #if ticker exists on local file then its valid        
-        else:
-            return True
+        #loops through the nested list to search for ticker
+        for valid_ticker in valid_tickers_list:
+            if self.ticker_symbol == valid_ticker[0]:
+                print('Found ticker on file.')
+                break
+            else:
+                #uses api search to see if the ticker exists 
+                url = (f"https://financialmodelingprep.com/api/v3/search-ticker?query={self.ticker_symbol}&limit=10&exchange=NASDAQ&apikey={self.API_KEY}")
+                data = Stock.get_jsonparsed_data(url)
+                print(data)
+                #returns [ ] list if cant find a ticker
+                if data == [ ]:
+                    print('Didn''t find ticker on API.')  
+                    break
+                else:
+                    tfm.write_file(self.ticker_symbol)
+                    print('Found ticker on API. Recording ticker to file.')
+                    break
             
         
