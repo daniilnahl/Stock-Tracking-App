8e919e977f108f738a7e632a7245b94e6a4ca1f7
daniilnahl <daniil.nahl@protonmail.com>
2024-11-08 20:01:54 -0800

    v0.00.1.3
    Created show_stocks function which utilizes Rich API to create a formatted table showcasing stocks.
    Moddifed stocks attribute so a new list is created with each instance of the object.

--- watch_list.py
+++ watch_list.py
@@ -1,10 +1,13 @@
 #CLASS FOR A WATCH LIST
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from rich.console import Console
+from rich.table import Table
 
 @dataclass
 class Watch_list:
     name: str
-    stocks: list = None
+    #use default_factory to intialize an empty list for each instance because without the list would used across all the class instances(big no no. Also without it you get a value error).
+    stocks: list = field(default_factory=list)
     
     def add_stock(self, stock: object):
         self.stocks.append(stock)
@@ -16,9 +19,28 @@ class Watch_list:
     def get_name(self):
         return self.name
     
-    @name.setter
+    @get_name.setter
     def change_name(self):
         new_name = input('Enter the new name of this watch list: ')  
         self.name = new_name
              
-    # def show_stocks(self):
+    def show_stocks(self):
+        #creates the table object
+        table = Table(title=f"{self.name}")
+        
+        #creates columns
+        table.add_column("Exchange", justify="right", style="magenta", no_wrap=True)
+        table.add_column("Ticker", style="magenta")
+        table.add_column("Company name", style="magenta")
+        table.add_column("Price", style="magenta")
+        table.add_column("Market Cap", style="magenta")
+        table.add_column("Sector", style="magenta")
+        table.add_column("Country", style="magenta")
+        
+        for stock in self.stocks: #calls stock attributes 
+           table.add_row(stock.exchange, stock.ticker_symbol, stock.name, stock.price, stock.market_cap, stock.sector,  stock.country) 
+           
+        #creates console and prints table
+        console = Console()
+        console.print(table)
+        
