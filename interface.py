d2e97a9ee0b99d5421255a4fdfe8b3b4bf81a0ce
daniilnahl <daniil.nahl@protonmail.com>
2024-11-20 17:57:45 -0800

    v.0.00.2.1
    Created full menu to control a watchlist. Created commands to add stocks and remove stocks from a watchlist.
    Create a command to show all stocks in a watchlist.
    Used pickle to store watchlist data externally and import it whenever needed (after completion of each command).

--- interface.py
+++ interface.py
@@ -1,40 +1,58 @@
-import typer
+import main #api key
+import typer #CLI
+
+#OBJECTS 
 from stock import Stock
 from watch_list import Watch_list
+
+#helper functions
+from utils import utility_module
+import pickle
+import os
+
+#functions to handle saving watchlist 
+def save_watchlist(watchlist):
+    with open(WATCHLIST_FILE, "wb") as f:
+        pickle.dump(watchlist, f)
+
+def load_watchlist():
+    if os.path.exists(WATCHLIST_FILE):
+        with open(WATCHLIST_FILE, "rb") as f:
+            return pickle.load(f)
+    return Watch_list("Watchlist")
+
+WATCHLIST_FILE = "watchlist.pkl" 
+current_watchlist = load_watchlist()#loads it from an external file
 
 app = typer.Typer()
 
 @app.command()
-def launch_watchlist() -> Watch_list:
-    #NEED to collect the name of watch list to a file
-    name = typer.prompt("How would you like to name your watch list?")
-    watchlist = Watch_list(name) #creates a watch list object
-    typer.echo(f"Created a watch list named {name}.\n")
-    typer.echo("""
-    COMMANDS FOR WATCH LIST
-    1. add-stock
-    2. remove-stock
-    3. show-stocks
-    4. change-name
-    """)
+def add_stock():
+    stock_ticker = (typer.prompt("Enter stock ticker")).upper()
+    stock_valid = utility_module.check_ticker(stock_ticker, main.API_KEY)
     
-    while True:
-        try:
-            command = int(typer.prompt('Enter command number'))
-            match command:
-                case 1:
-                    typer.echo("case 1")
-                case 2:
-                    typer.echo("case 2")
-                case 3:
-                    typer.echo("case 3")
-                case 4:
-                    typer.echo("case 4")
-                case _:
-                    typer.echo("Command doesn't exist.")
-            
-        except ValueError:
-            typer.echo("Please enter an integer number corresponding to a command.")
+    if stock_valid == False: #if stock ticker is invalid
+        typer.echo("Invalid ticker. Try again.")
+    else:#if stock ticker is valid we create an instance of the object and fill it up with data
+        stock = Stock(stock_ticker, main.API_KEY)   
+        stock.get_stock_info()
+        stock.get_price_over_time()
+        #record the stock instance into the watchlist
+        current_watchlist.add_stock(stock)
+        save_watchlist(current_watchlist)#updates the external file
+ 
+@app.command()  
+def delete_stock():
+    current_watchlist.show_just_tickers()
+    stock_ticker = (typer.prompt("Enter stock ticker")).upper()
+    current_watchlist.remove_stock(stock_ticker)#removes the stock
+    current_watchlist.show_just_tickers()
+    save_watchlist(current_watchlist)#updates the external file
+
+@app.command()
+def show_stocks():
+    current_watchlist.show_stocks()
+    
 
 if __name__ == "__main__":
     app()
